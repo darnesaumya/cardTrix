e21b279078b17db8d6e3cc5aa0f2d65fa2396a5d
Saumya Darne <darnesaumya@gmail.com>
2019-05-29 12:19:22 +0530

    Second Commit

--- app/src/main/java/com/example/cardtrix/MainActivity.java
+++ app/src/main/java/com/example/cardtrix/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.cardtrix;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -14,6 +17,8 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     RelativeLayout rl1;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
     TextView t1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +26,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         rl1 = findViewById(R.id.rl1);
         t1 = findViewById(R.id.t1);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
+        toggle = new ActionBarDrawerToggle(this , drawerLayout, R.string.Open, R.string.Close);
         rl1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent cardactivityintent = new Intent(MainActivity.this, CardViewActivity.class);
-                startActivity(cardactivityintent);
+                Intent cardActivityIntent = new Intent(MainActivity.this, CardViewActivity.class);
+                startActivity(cardActivityIntent);
             }
         });
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
