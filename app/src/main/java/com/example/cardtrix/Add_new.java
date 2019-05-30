219484e61abe4437f41b146082a36f3176701fdf
Saumya Darne <darnesaumya@gmail.com>
2019-05-31 00:44:10 +0530

    DB Test

--- app/src/main/java/com/example/cardtrix/Add_new.java
+++ app/src/main/java/com/example/cardtrix/Add_new.java
@@ -1,13 +1,25 @@
 package com.example.cardtrix;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Add_new extends AppCompatActivity {
-
+    EditText tf1,tf2,tf3,tf4,tf5,tf6,tf7;
+    SQLiteDatabase sqldb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new);
+        tf1 = findViewById(R.id.com_name);
+        tf2 = findViewById(R.id.emp1);
+        tf3 = findViewById(R.id.num1);
+        tf4 = findViewById(R.id.emp2);
+        tf5 = findViewById(R.id.num2);
+        tf6 = findViewById(R.id.email);
+        tf7 = findViewById(R.id.address);
+        sqldb = openOrCreateDatabase("MainDB",MODE_PRIVATE, null);
+
     }
 }
--- app/src/main/java/com/example/cardtrix/MainActivity.java
+++ app/src/main/java/com/example/cardtrix/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.cardtrix;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     RelativeLayout rl1;
     Toolbar toolbar;
     NavigationView navigationView;
+    SQLiteDatabase sqldb;
     private ActionBarDrawerToggle toggle;
     TextView t1;
     @Override
@@ -27,10 +29,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         navigationView = findViewById(R.id.nav_view);
         rl1 = findViewById(R.id.rl1);
         t1 = findViewById(R.id.t1);
+        sqldb = openOrCreateDatabase("MainDB", MODE_PRIVATE,null);
+        sqldb.execSQL("CREATE TABLE IF NOT EXISTS CardTable (C_Name VARCHAR , Emp1 VARCHAR, Num1 INTEGER, Emp2 VARCHAR, Num2 INTEGER, Email VARCHAR, Address VARCHAR)");
         toolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
         toggle = new ActionBarDrawerToggle(this , drawerLayout, R.string.Open, R.string.Close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        navigationView.setNavigationItemSelectedListener(this);
         rl1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,12 +49,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 startActivity(cardActivityIntent);
             }
         });
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-        if(getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        }
-        navigationView.setNavigationItemSelectedListener(this);
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -52,7 +57,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
         return super.onOptionsItemSelected(item);
     }
-
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         int id = menuItem.getItemId();
@@ -60,7 +64,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             Intent intent = new Intent(this, Add_new.class);
             startActivity(intent);
         }
-
         return true;
     }
 }
