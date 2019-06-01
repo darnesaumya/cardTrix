bf7645a7685af5c40ec78a67b4ed8cf3f3ad5552
Saumya Darne <darnesaumya@gmail.com>
2019-06-01 11:31:36 +0530

    LayoutInflator Test

--- app/src/main/java/com/example/cardtrix/MainActivity.java
+++ app/src/main/java/com/example/cardtrix/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cardtrix;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -9,6 +10,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
@@ -17,39 +19,27 @@ import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    RelativeLayout rl1;
     Toolbar toolbar;
     NavigationView navigationView;
     SQLiteDatabase sqldb;
     private ActionBarDrawerToggle toggle;
-    TextView t1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         navigationView = findViewById(R.id.nav_view);
-        rl1 = findViewById(R.id.rl1);
-        t1 = findViewById(R.id.t1);
         sqldb = openOrCreateDatabase("MainDB", MODE_PRIVATE,null);
         sqldb.execSQL("CREATE TABLE IF NOT EXISTS CardTable (C_Name VARCHAR , Emp1 VARCHAR, Num1 INTEGER, Emp2 VARCHAR, Num2 INTEGER, Email VARCHAR, Address VARCHAR)");
         toolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
         toggle = new ActionBarDrawerToggle(this , drawerLayout, R.string.Open, R.string.Close);
-
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         if(getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
         navigationView.setNavigationItemSelectedListener(this);
-        rl1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent cardActivityIntent = new Intent(MainActivity.this, CardViewActivity.class);
-                startActivity(cardActivityIntent);
-            }
-        });
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -74,7 +64,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Cursor resultset = sqldb.rawQuery("Select C_Name from CardTable",null);
         resultset.moveToLast();
         String str = resultset.getString(0);
-        t1.setText(str);
         resultset.close();
+    }
+    private void populator()
+    {
+        LayoutInflater v1 = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = (View) v1.inflate(R.layout.card_layout,null);
     }
 }
