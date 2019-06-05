62dd272433a05182b1cc1ce8b8c4b77c953edbd4
Saumya Darne <darnesaumya@gmail.com>
2019-06-05 19:55:46 +0530

    populator() working

--- app/src/main/java/com/example/cardtrix/MainActivity.java
+++ app/src/main/java/com/example/cardtrix/MainActivity.java
@@ -10,36 +10,42 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.support.v7.widget.Toolbar;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     Toolbar toolbar;
     NavigationView navigationView;
-    SQLiteDatabase sqldb;
+    SQLiteDatabase sql;
+    LinearLayout mainLayout;
+    DrawerLayout drawerLayout;
+    View tempView;
     private ActionBarDrawerToggle toggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         navigationView = findViewById(R.id.nav_view);
-        sqldb = openOrCreateDatabase("MainDB", MODE_PRIVATE,null);
-        sqldb.execSQL("CREATE TABLE IF NOT EXISTS CardTable (C_Name VARCHAR , Emp1 VARCHAR, Num1 INTEGER, Emp2 VARCHAR, Num2 INTEGER, Email VARCHAR, Address VARCHAR)");
         toolbar = findViewById(R.id.my_toolbar);
-        setSupportActionBar(toolbar);
-        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        drawerLayout = findViewById(R.id.drawerLayout);
         toggle = new ActionBarDrawerToggle(this , drawerLayout, R.string.Open, R.string.Close);
         drawerLayout.addDrawerListener(toggle);
+        setSupportActionBar(toolbar);
         toggle.syncState();
         if(getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+        sql = openOrCreateDatabase("MainDB", MODE_PRIVATE,null);
+        sql.execSQL("CREATE TABLE IF NOT EXISTS CardTable (C_Name VARCHAR , Emp1 VARCHAR, Num1 INTEGER, Emp2 VARCHAR, Num2 INTEGER, Email VARCHAR, Address VARCHAR)");
         navigationView.setNavigationItemSelectedListener(this);
+        populator();
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -61,14 +67,28 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     protected void onResume() {
         super.onResume();
-        Cursor resultset = sqldb.rawQuery("Select C_Name from CardTable",null);
-        resultset.moveToLast();
-        String str = resultset.getString(0);
-        resultset.close();
+        mainLayout = findViewById(R.id.linear_layout);
+        mainLayout.removeAllViews();
+        populator();
     }
-    private void populator()
-    {
-        LayoutInflater v1 = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = (View) v1.inflate(R.layout.card_layout,null);
+    private void populator() {
+        mainLayout = findViewById(R.id.linear_layout);
+        List<String> mainList = new ArrayList<>();
+        Cursor resultset = sql.rawQuery("Select C_Name from CardTable", null);
+        if(resultset.getCount() > 0){
+             resultset.moveToFirst();
+            do {
+                mainList.add(resultset.getString(0));
+            }while (resultset.moveToNext());
+    }
+        LayoutInflater li =  (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        for (int i = 0; i < mainList.size();  i++){
+            tempView = li.inflate(R.layout.card_layout, null);
+            TextView textMain = tempView.findViewById(R.id.tf);
+            textMain.setText(mainList.get(i));
+            mainLayout.addView(tempView);
+        }
+        mainList.clear();
+        resultset.close();
     }
 }
